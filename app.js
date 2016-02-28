b7d1b6fcf6f271584c6f8821ae2efcf1426874d9
Robin.Edbom@gmail.com <Robin.Edbom@gmail.com>
2016-02-28 20:38:23 +0100

    Now works with GET /Series and GET /Series/id

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 // BASE SETUP
 // =============================================================================
 
+var MongoClient = require('mongodb').MongoClient;
+var db;
+
 // call the packages we need
 var express = require('express');        // call express
 var app = express();                 // define our app using express
@@ -19,10 +22,58 @@ var port = process.env.PORT || 8080;        // set our port
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api/ImdbService/v1/', require("./routes/api"));
+var routerImdbService = require('express').Router();
+
+app.use('/api/ImdbService/v1/', routerImdbService);
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
 
+MongoClient.connect("mongodb://localhost:27017/api", function (err, database) {
+    if (!err) {
+        console.log("We are connected");
+        app.listen(port);
+        console.log('Magic happens on port ' + port);
+        db = database;
+
+    } else {
+        console.log("not connected!");
+    }
+});
+
+
+routerImdbService.get("/Series", function (req, res) {
+    db.collection('imdb').find({}).toArray(function (err, items) {
+
+        var seriesArray = [];
+
+        for (var i = 0; i < items.length; i++) {
+            var currentItem = items[i];
+
+            var series = {
+                id: currentItem._id,
+                title: currentItem.title
+            };
+
+            seriesArray.push(series);
+        }
+
+
+        res.json(seriesArray);
+    });
+});
+
+
+routerImdbService.get("/Series/:id", function (req, res) {
+    var id = req.params.id;
+    db.collection('imdb').findOne({_id: id}, function (err, item) {
+        if (!item)
+            item = {};
+        res.json(item);
+    });
+});
+
+
+var findRestaurants = function (db, callback) {
+
+};
