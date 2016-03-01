bc9beee966fc8f7fc434b101617ca53c9f3bc558
Robin.Edbom@gmail.com <Robin.Edbom@gmail.com>
2016-03-01 01:19:45 +0100

    Tried to make it more readable

--- app.js
+++ app.js
@@ -71,5 +71,8 @@ var printRequestInfo = function (req) {
     var path = "\nPath : " + req.path;
     var payLoad = req.method === "POST" || req.method === "PUT" ? "\n Payload : " + JSON.stringify(req.body) : "";
 
-    console.log(method + path + payLoad);
+    var date = new Date();
+    var time = "\nTime " + date.getMonth() + 1 + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    console.log(method + path + payLoad + time);
 };
